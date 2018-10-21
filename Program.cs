c414c44a09157684014f90a8cbb0ad3f7abd715c
D-A-I <simple_life_dev@yahoo.co.jp>
2018-10-22 01:35:12 +0900

    初回コミット

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trial_and_error_1022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
