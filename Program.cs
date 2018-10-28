009e78b008e0e2857745a5ea15168b8103378c03
D-A-I <simple_life_dev@yahoo.co.jp>
2018-10-28 23:56:37 +0900

    appsettings.jsonの読込み追加

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace trial_and_error_1022
 {
+    /// <summary>
+    /// MySqlにCRUDする（GET / POST / PUT / DELETE）
+    /// ・MySqlをインストールする -> OK
+    /// ・MySqlWorkbenchをインストールする -> OK
+    /// ・MySqlにテーブルを作成する -> OK
+    /// ・MySqlにデータを投入する -> (一旦)OK
+    /// ・別プロジェクトを作る -> OK
+    /// ・MySqlからscaffoldする
+    /// ・DIで接続文字列を渡す
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = GetConfiguration();
+            var conStr = config.GetConnectionString("kurumiDB");
+
+            Console.WriteLine(conStr);
+        }
+
+        /// <summary>
+        /// ConfigurationBuilderの作成
+        /// </summary>
+        /// <returns></returns>
+        static IConfiguration GetConfiguration()
+        {
+            var configBuilder = new ConfigurationBuilder();
+            
+            // appsettings.jsonへのパスを通す
+            configBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            configBuilder.AddJsonFile(@"appsettings.json");
+            
+            return configBuilder.Build();
         }
     }
 }
