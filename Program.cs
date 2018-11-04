a655af76d2b720a617e429c49462429f96527433
D-A-I <simple_life_dev@yahoo.co.jp>
2018-11-04 21:59:13 +0900

    少しだけ更新

--- Program.cs
+++ Program.cs
@@ -2,41 +2,31 @@
 using System.IO;
 using Microsoft.Extensions.Configuration;
 
-namespace trial_and_error_1022
-{
+namespace trial_and_error_1022 {
     /// <summary>
-    /// MySqlにCRUDする（GET / POST / PUT / DELETE）
-    /// ・MySqlをインストールする -> OK
-    /// ・MySqlWorkbenchをインストールする -> OK
-    /// ・MySqlにテーブルを作成する -> OK
-    /// ・MySqlにデータを投入する -> (一旦)OK
-    /// ・別プロジェクトを作る -> OK
-    /// ・MySqlからscaffoldする
-    /// ・DIで接続文字列を渡す
+    /// APIクライアントを作成する
+    /// MySqlへCRUDする（GET / POST / PUT / DELETE）
     /// </summary>
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var config = GetConfiguration();
-            var conStr = config.GetConnectionString("kurumiDB");
+    class Program {
+        static void Main (string[] args) {
+            var config = GetConfiguration ();
+            var conStr = config.GetConnectionString ("kurumiDB");
 
-            Console.WriteLine(conStr);
+            Console.WriteLine (conStr);
         }
 
         /// <summary>
         /// ConfigurationBuilderの作成
         /// </summary>
         /// <returns></returns>
-        static IConfiguration GetConfiguration()
-        {
-            var configBuilder = new ConfigurationBuilder();
-            
+        static IConfiguration GetConfiguration () {
+            var configBuilder = new ConfigurationBuilder ();
+
             // appsettings.jsonへのパスを通す
-            configBuilder.SetBasePath(Directory.GetCurrentDirectory());
-            configBuilder.AddJsonFile(@"appsettings.json");
-            
-            return configBuilder.Build();
+            configBuilder.SetBasePath (Directory.GetCurrentDirectory ());
+            configBuilder.AddJsonFile (@"appsettings.json");
+
+            return configBuilder.Build ();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # trial-and-error_1022
+## このアプリの進め方
+### APIクライアント（C#.. vscodeで作成）
+1. MySqlにテーブルを作成する  
+-> char setをutf-8に変えて再作成する  
+-> OK
+1. MySqlにデータを投入する  
+-> OK
+1. 別プロジェクトを作る  
+-> OK
+1. MySqlからscaffoldする  
+-> 着手中
+-> メモ..  
+-> @"server=localhost;database=kurumi;userid=developer;pwd=xxxxx;sslmode=none;"
+1. DIで接続文字列を渡す
+1. 環境構築シェルスクリプトを作る  
+  - 親ディレクトリ名を取得してslnファイルを作る
+  - 親ディレクトリ名.Cli フォルダを作成し、"console"（実際はWebかつMVC）テンプレを追加
+  - 親ディレクトリ名.Models フォルダを作成し、"classlib"テンプレを追加
+  - slnと子csprojを紐付けする
+  - Cliプロジェクトにappsettings.json用の各種パッケージを追加  
+  参考） https://qiita.com/abpla/items/c4faeed42bef4d450c8d
+  - ModelプロジェクトにMySqlからscaffoldしたモデルを追加する  
+  参考） https://qiita.com/takanemu/items/ebca534db398aa9cce34
+---
+## （参考）mysqlについて
+### mysqlのインストール
+http://ksino.hatenablog.com/entry/2016/11/17/232619
+### 初回ログイン時
+```bash
+$ sudo mysql -u root -p
+```
+sudoで、ubuntuの管理者パスワードを入力する
+### ユーザの作成
+```sql
+CREATE USER 'developer'@'localhost' IDENTIFIED BY 'xxxxx';
+```
+> 参考  
+> https://www.dbonline.jp/mysql/user/index1.html
+
+### 権限付与
+https://qiita.com/shuntaro_tamura/items/2fb114b8c5d1384648aa  
+### character setの設定
+＊ 必ずdatabaseを作成する前に設定
+> 参考  
+> https://qiita.com/ldap2017/items/4d172128960218b00ab5
+
+なお、設定ファイルのmy.cnfは、ルートディレクトリから探すと見付かる
+```bash
+cd /
+```
+### mysqlのアンインストール
+```bash
+$ sudo service mysql stop
+$ sudo killall -9 mysql
+$ sudo killall -9 mysqld
+$ sudo apt-get remove --purge mysql-server mysql-client mysql-common
+$ sudo apt-get autoremove
+$ sudo apt-get autoclean
+$ sudo deluser mysql
+$ sudo rm -rf /var/lib/mysql
+$ sudo apt-get purge mysql-server-core-5.5
+$ sudo apt-get purge mysql-client-core-5.5
+$ sudo rm -rf /var/log/mysql
+$ sudo rm -rf /etc/mysql
+```
